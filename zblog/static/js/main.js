f3369f3e05317190b08749e7f7f07a644f49f5e6
sebsepia <account@sebsepia.org>
2020-11-29 00:55:47 -0500

    integrate linktree

--- zblog/static/js/main.js
+++ zblog/static/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+  /* stops Posts from making images stretch by having too much text  */
+
+  var alignTextToImg = function(){
+      var setter;
+      $("div.intime").each(function(obj){
+        var setter = $(this).find("img");
+        var target = $(this).find("p");
+        target.css("max-width", setter.width()+"px");
+        console.log(setter.width(), target.text());
+      });
+  };
+  alignTextToImg();
+
   /*isolates title letters in separate spans*/
   title = $("#titlecard");
   title.html(function(index, html) {
