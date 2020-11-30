eb09c337d48872cb1af1ce373f89218e890bfb4f
sebsepia <account@sebsepia.org>
2020-11-30 23:07:59 +0000

    kess ca change

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
 
 
-  const linkTree = $('.linktree');
-
-  $('#expandbtn', '#collapsebtn').click(function()  {
-    console.log("hey atleast it works");
+  $('#collapsebtn').click(function()  {
+   console.log("hey atleast it works");
+   $('.linktree').toggleClass('switch');
   });
-
 
   /*isolates title letters in separate spans*/
   title = $("#titlecard");
@@ -36,3 +34,14 @@ $(document).ready(function () {
     });
 
 });
+  /* stops Posts from making images stretch by having too much text  */
+/*  var alignTextToImg = function(){
+      var setter;
+      $("div.intime").each(function(obj){
+        var target = $(this).find("p");
+        var setter = target.find("img");
+        target.css("max-width", setter.width()+"px");
+        console.log(setter.width(), target.text());
+      });
+  };
+  alignTextToImg();*/
