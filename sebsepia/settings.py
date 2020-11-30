231f768b2642d37a8a656b212cf4c7f7b7a1d433
sebsepia <account@sebsepia.org>
2020-11-30 17:47:35 -0500

    edited settings.py

--- sebsepia/settings.py
+++ sebsepia/settings.py
@@ -16,7 +16,6 @@ MEDIA_ROOT = MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'k%)!nt)wy!ky%f9%o4%r_*eghh8jpmrw!#1afcg@_*v4+@stag'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
