394c8d687ec718ad97a91abd45ae608ba8a06225
sebsepia <account@sebsepia.org>
2021-01-09 13:37:35 -0500

    changement de base html

--- zblog/static/zblog/js/main.js
+++ zblog/static/zblog/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
 
   /*Creates simpler preview of posts in  Portfolio */
   $('div.portfoliointime').each(function (){
-    $('img').not('img:first-of-type').hide();
+    $('img').not('img:first').hide();
     $('h4').hide();
   });
 
